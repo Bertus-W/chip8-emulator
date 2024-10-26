fe5ed67380878a00625d6a5a1e97af3642a6d763
b.wisman155 <b.wisman155@gmail.com>
2024-10-26 23:43:15 +0200

    fix for display

--- src/main.cpp
+++ src/main.cpp
@@ -301,7 +301,7 @@ int main(int argc, char *argv[]) {
   SetTargetFPS(60);
 
   // Initialize CHIP-8 components
-  bool display[DISPLAY_HEIGHT][DISPLAY_WIDTH];
+  bool display[DISPLAY_HEIGHT][DISPLAY_WIDTH] = {};
   uint8_t *memory = (uint8_t *)calloc(MEMORY_SIZE, sizeof(uint8_t));
   uint8_t *Vx = (uint8_t *)calloc(VX_SIZE, sizeof(uint8_t));
   uint16_t *stack = (uint16_t *)calloc(STACK_SIZE, sizeof(uint16_t));
